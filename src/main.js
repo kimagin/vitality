047bc5b210f0d7291d5f6fcb1aa78f6a2783a83c
kimagin <inkimiaei@gmail.com>
2022-06-06 16:18:32 +0200

    Adding the following dependencies :     - Tailwind preflight resets
    Customizing the scroll bar

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "@unocss/reset/tailwind.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "uno.css";
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Unocss from "unocss/vite";
 
+import { presetAttributify, presetUno } from "unocss";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetUno(), presetAttributify()],
+    }),
+  ],
 });
