2bda82ba74dd779c487bf6ca9302b8d3fa32db6e
kimagin <inkimiaei@gmail.com>
2022-06-06 16:29:21 +0200

    Solving linter errors

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template >
-  <img
-    class="absolute object-cover w-full h-full top-30 -z-1"
-    src="https://images.unsplash.com/photo-1638438133972-1e61d6114763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80"
-    alt=""
-  />
-  <v-welcome class="z-10 text-light-100"></v-welcome>
+  <div>
+    <img
+      class="absolute object-cover w-full h-full top-30 -z-1"
+      src="https://images.unsplash.com/photo-1638438133972-1e61d6114763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80"
+      alt=""
+    />
+    <v-welcome class="z-10 text-light-100"></v-welcome>
+  </div>
 </template>
 
 
