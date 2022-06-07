82815427559a68d0896d9c19123cafaaf4b88660
kimagin <inkimiaei@gmail.com>
2022-06-07 15:34:40 +0200

    Implementing animation on the sliders. There is a known issue which is a bug in chrome regarding the simultinious usage of backdrop blur and css transition which causes a conflict.

--- src/components/v-carousel.vue
+++ src/components/v-carousel.vue
@@ -1,72 +1,64 @@
 <template>
-  <section class="relative w-full h-full min-w-[100rem] sm:scale-125 -z-20">
-    <div
-      class="
-        absolute
-        flex flex-col
-        items-center
-        justify-start
-        font-mono
-        top-1/2
-        left-1/2
-        -translate-1/2
-        sm:rounded-xl
-        backdrop-blur-md
-        drop-shadow-2xl
-        bg-gradient-to-br
-        sm:w-auto
-        w-[100vw]
-        from-emerald-300/20
-        to-teal-600/30
-        text-light-300
-        h-50
-        sm:h-auto
-      "
-    >
-      <div
-        class="top-0 left-0 flex w-full py-3 pl-1 border border-transparent  border-b-emerald-400/10"
-      >
-        <span
-          v-for="circle in 3"
-          class="block w-3 h-3 ml-2 rounded-full bg-emerald-800"
-          :key="circle"
-        ></span>
-      </div>
-      <div
-        class="flex flex-col items-center justify-around h-full px-8 py-5 text-sm  sm:text-sm"
-      >
+  <section class="relative w-full h-max sm:scale-100 -z-20 min-h-35">
+    <div class="w-full h-full mx-auto">
+      <transition-group name="slide-fade">
         <span
           v-for="slide in slides"
           :key="slide.id"
           v-show="slide.isActive === true"
+          class="absolute left-0 right-0"
         >
-          <div class="flex flex-col">
-            <div class="text-gray-400">
-              {{ slide.title }}
+          <div
+            class="flex flex-col items-center justify-start mx-auto font-mono  w-max sm:rounded-xl backdrop-blur-md drop-shadow-2xl bg-gradient-to-br from-emerald-300/20 to-teal-600/30 text-light-300"
+          >
+            <div
+              class="top-0 left-0 flex w-full py-3 pl-1 border border-transparent  border-b-emerald-400/10"
+            >
+              <span
+                v-for="circle in 3"
+                class="block w-3 h-3 ml-2 rounded-full bg-emerald-800"
+                :key="circle"
+              ></span>
             </div>
-            <div>
-              <span class="text-orange-400">{{ slide.server + " " }}</span>
-              <span>{{ slide.action || "" + " " }}</span>
-              <a :href="slide.value" target="_blank">
-                <span class="text-emerald-400"> {{ slide.value + " " }}</span>
-                <span class="text-cyan-500">{{ slide.desctiption }}</span>
-              </a>
+
+            <div
+              class="flex flex-col items-center justify-around block w-full h-full px-8 py-5 text-sm  w-min sm:text-sm"
+            >
+              <span class="whitespace-nowrap">
+                <div class="flex flex-col">
+                  <div class="text-gray-400">
+                    {{ slide.title }}
+                  </div>
+                  <div>
+                    <span class="text-orange-400">{{
+                      slide.server + " "
+                    }}</span>
+                    <span>{{ slide.action || "" + " " }}</span>
+                    <a :href="slide.value" target="_blank">
+                      <span class="text-emerald-400">
+                        {{ slide.value + " " }}</span
+                      >
+                      <span class="text-cyan-500">{{ slide.desctiption }}</span>
+                    </a>
+                  </div>
+                </div>
+              </span>
             </div>
           </div>
         </span>
-      </div>
-      <div class="">
-        <div
-          class="relative left-0 flex items-center justify-center w-full h-full space-x-2  top-4 pagination"
-        >
-          <span
-            v-for="slide in slides"
-            :key="slide.id"
-            class="w-2 h-2 bg-teal-400 rounded-full cursor-pointer"
-            :class="slide.isActive === false ? 'opacity-40' : ''"
-            @click="page(slide)"
-          ></span>
-        </div>
+      </transition-group>
+    </div>
+    <div class="absolute top-33 w-full mx-auto]">
+      <div
+        class="flex items-center justify-center w-full h-full space-x-2  pagination"
+      >
+        <span
+          v-for="slide in slides"
+          :key="slide.id"
+          class="w-2 h-2 bg-teal-400 rounded-full cursor-pointer"
+          :class="slide.isActive === false ? 'opacity-40' : ''"
+          @click="page(slide)"
+        ></span>
       </div>
     </div>
   </section>
@@ -125,4 +117,16 @@ export default {
 </script>
 
 <style>
+.slide-fade-enter-active {
+  transition: opacity 0.5s ease;
+}
+
+.slide-fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0;
+}
 </style>
