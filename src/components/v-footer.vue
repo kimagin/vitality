5a024a3496435da3ba5e88194ca1a3070a59080c
kimagin <inkimiaei@gmail.com>
2022-06-07 03:25:20 +0200

    Micro refining the design and changing the footer's style.

--- src/components/v-footer.vue
+++ src/components/v-footer.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex flex-col items-center justify-center mb-[2%]">
+  <div class="flex flex-col items-center justify-center w-full">
+    <span class="w-full h-[1px] rounded-full bg-emerald-500 opacity-10"></span>
     <h3 class="text-sm tracking-wide text-center text-warm-gray-500"></h3>
-    <h2 class="tracking-wide text-center text-.8rem text-warm-gray-500">
+    <h2 class="tracking-wide text-center text-.8rem text-warm-gray-500 my-10">
       GNU Licensed | presented by Kimagination
     </h2>
   </div>
